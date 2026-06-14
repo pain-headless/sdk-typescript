42077e5d948e9fc94f47d5c685fa9a74c038f663
pain-headless <contact@painheadless.com>
2026-06-14 15:10:49 -0300

    feat: introduce custom error classes for package exceptions and export them in the main entry point

---
+++ src/exceptions.ts
@@ -0,0 +1,64 @@
+export class PainHeadlessError extends Error {
+
+    constructor(message: string, public code?: number, public partialData?: any, public string?: errorCause) {
+        super(message);
+        this.name = this.constructor.name;
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export class ConnectionError extends PainHeadlessError {
+    constructor(message: string = 'Failed to connect to the WebSocket server.') {
+        super(message, -2);
+    }
+}
+
+export class DisconnectedError extends PainHeadlessError {
+    constructor(message: string = 'The WebSocket connection was closed unexpectedly.') {
+        super(message, -3);
+    }
+}
+
+export class AuthenticationError extends PainHeadlessError {
+    constructor(message: string = 'Authentication failed. Please check your API Key.') {
+        super(message, 3);
+    }
+}
+
+export class PayloadParsingError extends PainHeadlessError {
+    constructor(message: string = 'Failed to parse the incoming WebSocket message.') {
+        super(message, 1000);
+    }
+}
+
+export class TimeoutError extends PainHeadlessError {
+    constructor(message: string = 'The operation timed out.') {
+        super(message, 1001);
+    }
+}
+
+export class InsufficientCreditsError extends PainHeadlessError {
+    constructor(message: string = 'Insufficient credits to perform this action.') {
+        super(message, 5);
+    }
+}
+
+export class InvalidParamsError extends PainHeadlessError {
+    constructor(message: string = 'Invalid parameters provided for command.') {
+        super(message, 4);
+    }
+}
+
+export class CommandNotFoundError extends PainHeadlessError {
+    constructor(message: string = 'The requested command does not exist.') {
+        super(message, 2);
+    }
+}
+
+export class CommandError extends PainHeadlessError {
+    constructor(message: string = 'Error running the command.', errorCause?: string, partialData?: any) {
+        super(message, 1000, partialData, errorCause);
+    }
+}
