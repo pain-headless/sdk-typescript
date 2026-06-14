5aedc631ea0ab3301901e210ac47b53387647ed8
pain-headless <contact@painheadless.com>
2026-06-14 15:10:49 -0300

    chore: update production and local websocket base URLs

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const PROD_WS_URL = 'wss://api.painheadless.com/ws';
-export const LOCAL_WS_URL = 'ws://localhost:3000/ws';
+export const PROD_WS_URL = 'wss://api.painheadless.com';
+export const LOCAL_WS_URL = 'ws://localhost:5001';
