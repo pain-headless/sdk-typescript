be8eb90a44194c7e57b5f321a05a7c075a7eeede
pain-headless <contact@painheadless.com>
2026-06-14 15:10:49 -0300

    feat: implement WebSocket client with configurable environment URLs

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const PROD_WS_URL = 'wss://api.painheadless.com/ws';
+export const LOCAL_WS_URL = 'ws://localhost:3000/ws';
---
+++ src/ws-client.ts
@@ -0,0 +1,48 @@
+import { LOCAL_WS_URL, PROD_WS_URL } from "./config";
+import { WebSocket } from 'ws';
+
+export class WSClient {
+    private url: string;
+    private ws?: WebSocket;
+    private pendingResolve: any = null;
+    private pendingReject: any = null;
+
+    constructor (env: 'local'|'prod' = 'prod') {
+        this.url = {
+            'local': LOCAL_WS_URL,
+            'prod': PROD_WS_URL,
+        }[env];
+    }
+
+    connect () {
+        this.ws = new WebSocket(this.url);
+    
+        this.ws.on('message', (rawData) => {
+            try {
+                const response = JSON.parse(rawData.toString());
+    
+                if (this.pendingResolve && this.pendingReject) {
+                    this.pendingResolve(response);
+                }
+            } catch (err) {
+                this.pendingReject(err);
+            } finally {
+                delete this.pendingResolve;
+                delete this.pendingReject;
+            }
+        });
+    }
+
+    send(data: Record<string, any>): Promise<any> {
+        if (!this.ws || !(this.ws instanceof WebSocket)) {
+            throw new Error('WebSocket not connected');
+        }
+        
+        return new Promise((resolve, reject) => {
+            this.pendingResolve = resolve;
+            this.pendingReject = reject;
+
+            this.ws?.send(JSON.stringify(data));
+        });
+    }
+}
